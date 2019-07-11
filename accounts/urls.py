f54e4b0a2aa333b282bdcbef386599b5e245db7a
Hammad Malik <tomatohammado@gmail.com>
2019-07-11 13:05:49 -0400

    Finish Authentication + Authorization

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,11 +3,11 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('accounts/login',
+    path('accounts/login/',
          auth_views.LoginView.as_view(template_name='accounts/login.html'),
          name='login'),
-    path('accounts/logout',
+    path('accounts/logout/',
          auth_views.LogoutView.as_view(template_name='accounts/logout.html'),
          name='logout'),
-    path('accounts/signup', views.sign_up, name='signup'),
+    path('accounts/signup/', views.sign_up, name='signup'),
 ]
