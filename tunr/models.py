7d70a786baa2a0527980c4a42890740e3f979185
Hammad Malik <tomatohammado@gmail.com>
2019-10-10 12:18:01 -0400

    Make Artist model and associated migration

--- tunr/models.py
+++ tunr/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Artist(models.Model):
+    name = models.CharField(max_length=100)
+    nationality = models.CharField(max_length=100)
+    photo_url = models.TextField()
+
+    def __str__(self):
+        # return super().__str__()
+        return self.name
--- tunr_django/settings.py
+++ tunr_django/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tunr'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'tunr_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tunr',
+        'USER': 'tunruser',
+        'PASSWORD': 'tunr',
+        'HOST': 'localhost',
     }
 }
 
