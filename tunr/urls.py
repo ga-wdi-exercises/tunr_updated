721f98effa0faa4cd99910236690fede4c402b03
Hammad Malik <tomatohammado@gmail.com>
2019-10-10 16:56:25 -0400

    End of afternoon lesson

---
+++ tunr/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.artist_list, name='artist_list'),
+    path('artist/<int:pk>', views.artist_detail, name='artist_detail'),
+    path('songs', views.song_list, name='song_list'),
+]
--- tunr/views.py
+++ tunr/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Artist, Song
 
 # Create your views here.
+
+
+def artist_list(request):
+    artists = Artist.objects.all()
+    return render(request, 'tunr/artist_list.html', {'artists': artists})
+
+
+def artist_detail(request, pk):
+    artist = Artist.objects.get(id=pk)
+    return render(request, 'tunr/artist_detail.html', {'artist': artist})
+
+
+def song_list(request):
+    songs = Song.objects.all()
+    return render(request, 'tunr/song_list.html', {'songs': songs})
