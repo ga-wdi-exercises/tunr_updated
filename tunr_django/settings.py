60a75166a06765fc9f04ee95f4400449431154f9
Hammad Malik <tomatohammado@gmail.com>
2019-10-10 12:33:19 -0400

    Install django-extensions package, add to settings.py

--- tunr_django/settings.py
+++ tunr_django/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tunr'
+    'tunr',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
