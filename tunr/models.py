159d20b82593d216d037d938c3701c28ded9d939
Hammad Malik <tomatohammado@gmail.com>
2019-10-10 12:25:17 -0400

    Create Song model and associated migration

--- tunr/models.py
+++ tunr/models.py
@@ -11,3 +11,13 @@ class Artist(models.Model):
     def __str__(self):
         # return super().__str__()
         return self.name
+
+
+class Song(models.Model):
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='songs')
+    title = models.CharField(max_length=100, default='no song title')
+    album = models.CharField(max_length=100, default='no album title')
+    preview_url = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.title
