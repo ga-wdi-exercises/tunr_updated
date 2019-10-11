89f072ccd33333c9f2f05c7b4179533d28a80fe4
Hammad Malik <tomatohammado@gmail.com>
2019-10-11 11:28:17 -0400

    Finish codealong

---
+++ tunr/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import Artist, Song
+
+
+class ArtistForm(forms.ModelForm):
+
+    class Meta:
+        model = Artist
+        fields = ('name', 'photo_url', 'nationality',)
--- tunr/urls.py
+++ tunr/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.artist_list, name='artist_list'),
     path('artist/<int:pk>', views.artist_detail, name='artist_detail'),
+    path('artist/new', views.artist_create, name='artist_create'),
+    path('artist/<int:pk>/edit', views.artist_edit, name='artist_edit'),
+    path('artist/<int:pk>/delete', views.artist_delete, name='artist_delete'),
     path('songs', views.song_list, name='song_list'),
+    path('songs/<int:pk>', views.song_detail, name='song_detail'),
 ]
