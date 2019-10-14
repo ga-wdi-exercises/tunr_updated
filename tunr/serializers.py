e72465bd4b70e68c14f42f4c7e948b1eeecfde2b
Hammad Malik <tomatohammado@gmail.com>
2019-10-14 11:53:23 -0400

    Finish practice

--- tunr/serializers.py
+++ tunr/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Artist
+from .models import Artist, Song
 
 
 class ArtistSerializer(serializers.HyperlinkedModelSerializer):
@@ -9,6 +9,16 @@ class ArtistSerializer(serializers.HyperlinkedModelSerializer):
         read_only=True,
     )
 
+    artist_url = serializers.ModelSerializer.serializer_url_field(
+        view_name='artist_detail'
+    )
+
     class Meta:
         model = Artist
-        fields = ('id', 'photo_url', 'nationality', 'name', 'songs')
+        fields = ('id', 'artist_url', 'photo_url', 'nationality', 'name', 'songs',)
+
+
+class SongSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Song
+        fields = ('id', 'artist', 'title', 'album', 'preview_url',)
--- tunr/views.py
+++ tunr/views.py
@@ -1,84 +1,23 @@
-from django.shortcuts import render, redirect
-from django.http import JsonResponse
-
+from rest_framework import generics
+from .serializers import ArtistSerializer, SongSerializer
 from .models import Artist, Song
-from .forms import ArtistForm, SongForm
 
 
-def artist_list(request):
-    # artists = Artist.objects.all()
-    # return render(request, 'tunr/artist_list.html', {'artists': artists})
-    artists = Artist.objects.all().values('name', 'nationality', 'photo_url') # only grab some attributes from our database, else we can't serialize it.
-    artists_list = list(artists) # convert our artists to a list instead of QuerySet
-    return JsonResponse(artists_list, safe=False) # safe=False is needed if the first parameter is not a dictionary.
+class ArtistList(generics.ListCreateAPIView):
+    queryset = Artist.objects.all()
+    serializer_class = ArtistSerializer
 
 
-def song_list(request):
-    songs = Song.objects.all()
-    return render(request, 'tunr/song_list.html', {'songs': songs})
+class ArtistDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Artist.objects.all()
+    serializer_class = ArtistSerializer
 
 
-def artist_detail(request, pk):
-    artist = Artist.objects.get(id=pk)
-    return render(request, 'tunr/artist_detail.html', {'artist': artist})
+class SongList(generics.ListCreateAPIView):
+    queryset = Song.objects.all()
+    serializer_class = SongSerializer
 
 
-def song_detail(request, pk):
-    song = Song.objects.get(id=pk)
-    return render(request, 'tunr/song_detail.html', {'song': song})
-
-
-def artist_create(request):
-    if request.method == 'POST':
-        form = ArtistForm(request.POST)
-        if form.is_valid():
-            artist = form.save()
-            return redirect('artist_detail', pk=artist.pk)
-    else:
-        form = ArtistForm()
-    return render(request, 'tunr/artist_form.html', {'form': form})
-
-
-def song_create(request):
-    if request.method == 'POST':
-        form = SongForm(request.POST)
-        if form.is_valid():
-            song = form.save()
-            return redirect('song_detail', pk=song.pk)
-    else:
-        form = SongForm()
-    return render(request, 'tunr/song_form.html', {'form': form})
-
-
-def artist_edit(request, pk):
-    artist = Artist.objects.get(pk=pk)
-    if request.method == "POST":
-        form = ArtistForm(request.POST, instance=artist)
-        if form.is_valid():
-            artist = form.save()
-            return redirect('artist_detail', pk=artist.pk)
-    else:
-        form = ArtistForm(instance=artist)
-    return render(request, 'tunr/artist_form.html', {'form': form})
-
-
-def song_edit(request, pk):
-    song = Song.objects.get(pk=pk)
-    if request.method == "POST":
-        form = SongForm(request.POST, instance=song)
-        if form.is_valid():
-            artist = form.save()
-            return redirect('song_detail', pk=song.pk)
-    else:
-        form = SongForm(instance=song)
-    return render(request, 'tunr/song_form.html', {'form': form})
-
-
-def artist_delete(request, pk):
-    Artist.objects.get(id=pk).delete()
-    return redirect('artist_list')
-
-
-def song_delete(request, pk):
-    Song.objects.get(id=pk).delete()
-    return redirect('song_list')
+class SongDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Song.objects.all()
+    serializer_class = SongSerializer
