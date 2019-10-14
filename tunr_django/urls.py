10c0b595167850a1502b898b65f072ed9a4521a5
Hammad Malik <tomatohammado@gmail.com>
2019-10-14 11:18:26 -0400

    Add rest_framework settings

--- tunr_django/urls.py
+++ tunr_django/urls.py
@@ -20,4 +20,5 @@ from django.contrib import admin
 urlpatterns = [
     path('admin', admin.site.urls),
     path('', include('tunr.urls')),
+    path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
 ]
