2341e2ffb96450a2cf6c6b7b5849f7f227423217
Hammad Malik <tomatohammado@gmail.com>
2019-10-14 10:54:34 -0400

    Update JSON view method examples

--- tunr/views.py
+++ tunr/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render, redirect
+from django.http import JsonResponse
 
 from .models import Artist, Song
 from .forms import ArtistForm, SongForm
 
 
 def artist_list(request):
-    artists = Artist.objects.all()
-    return render(request, 'tunr/artist_list.html', {'artists': artists})
+    # artists = Artist.objects.all()
+    # return render(request, 'tunr/artist_list.html', {'artists': artists})
+    artists = Artist.objects.all().values('name', 'nationality', 'photo_url') # only grab some attributes from our database, else we can't serialize it.
+    artists_list = list(artists) # convert our artists to a list instead of QuerySet
+    return JsonResponse(artists_list, safe=False) # safe=False is needed if the first parameter is not a dictionary.
 
 
 def song_list(request):
